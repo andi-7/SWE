599142d6b1f148613cf069691f595549cfe1907d
andi-7 <andreas.starzer@gmx.at>
2018-04-20 20:58:46 +0000

    more code

--- 5_Listen/listen.h
+++ 5_Listen/listen.h
@@ -4,11 +4,13 @@
 //Einfach verkettete Liste 
 //(muss noch auf doppelt verkettet erweitert werden)
 
+
 /*SLIST Knoten*/
 typedef struct SList {
 	int *Data;
 	struct SList *Next;
 } SLIST;
+
 
 /*Kopf-Knoten, der die Listenlänge enthält*/
 typedef struct SList_Header {
