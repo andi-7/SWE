b6e4ffdc5fe2fa4db2a8b3680589414dd61499a6
andi <andi@andi-virtual-linux>
2018-04-23 22:52:10 +0200

    stack programm

---
+++ 6_Stack (LIFO)/src/stack.c
@@ -0,0 +1,65 @@
+/*
+ ============================================================================
+ Name        : stack.c
+ Author      : Andi
+ Version     :
+ Copyright   : Andreas Starzer
+ Description : Stack in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+
+NODE *push(NODE *head, int data) {
+	NODE *temp = malloc(sizeof(NODE));
+	temp->data = data;
+	temp->next = head;
+	head = temp;
+	return head;
+
+}
+
+NODE *pop(NODE *head, int *element) {
+	NODE *temp = head;
+	*element = head->data;
+	head = head->next;
+	free(temp);
+	return head;
+}
+
+void print(NODE *head) {
+	NODE *current;
+	current = head;
+
+	if (current != NULL) {
+		printf("Stack: ");
+		do {
+			printf("%d ", current->data);
+			current = current->next;
+		} while (current != NULL);
+		printf("\n");
+	} else {
+		printf("The Stack is empty\n");
+	}
+}
+
+int empty(struct node* head)
+{
+    return head == NULL ? 1 : 0;
+}
+
+int main(void) {
+
+		NODE *myStack=NULL;
+
+		myStack=push(myStack,7);
+		myStack=push(myStack,4);
+		myStack=push(myStack,9);
+
+		print(myStack);
+
+	    return 0;
+}
---
+++ 6_Stack (LIFO)/src/stack.h
@@ -0,0 +1,19 @@
+/*
+ * stack
+ *
+ *  Created on: 23.04.2018
+ *      Author: andi
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef STACK_H_
+#define STACK_H_
+
+typedef struct node{
+	struct node *next;
+	int data;
+}NODE;
+
+
+#endif /* STACK_H_ */
